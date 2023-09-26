90a2e6b5137bc2be8df40c5145a465f78da65e2e
Nathan Bourgeois <IridescentRosesFall@gmail.com>
2023-09-25 22:34:35 -0400

    Change Base File interface
    The base file contains purely unimplemented methods which fail loudly. The PAL (factory) will select the correct file implementation and use it.

---
+++ src/Utilities/Filesystem/CFile.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <Utilities/Filesystem/File.hpp>
+#include <cstdio>
+
+namespace CrossFire::detail {
+
+/**
+ * @brief File is an interface for file objects.
+ */
+class CFile final : public FileBase {
+    public:
+	CFile(const char *filename, const char *mode);
+	explicit CFile(FILE* file);
+	~CFile() override;
+
+	auto read(Slice<u8> &buffer) -> usize override;
+	auto write(const Slice<u8> &buffer) -> usize override;
+	auto size() -> isize override;
+
+	auto flush() -> void override;
+	auto close() -> void override;
+};
+
+}
